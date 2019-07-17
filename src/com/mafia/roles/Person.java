acdaa2f43e9b27fecd93c3d7a412b263baf66990
mohalisad <msadraei@icloud.com>
2019-07-17 23:23:12 +0430

    refactoring

--- src/com/mafia/roles/Person.java
+++ src/com/mafia/roles/Person.java
@@ -93,43 +93,7 @@ public abstract class Person {
     }
 
     public String getTypeInString(){
-        switch (type){
-            case KAVEH:
-                return "Kaveh";
-            case GHAREN:
-                return "Gharen";
-            case GUDARZ:
-                return "Gudarz";
-            case ROSTAM:
-                return "Rostam";
-            case MANZAR_BANU:
-                return "Manzar Banu";
-            case FAROKH_LAGHA:
-                return "Farokh Lagha";
-            case GHAMAR_VAZIR:
-                return "Ghamar Vazir";
-            case TUS:
-                return "Tus";
-            case ZALL:
-                return "Zall";
-            case KAMUS:
-                return "Kamus";
-            case SHAGHAD:
-                return "Shaghad";
-            case AFRASIAB:
-                return "Afrasiab";
-            case GARSIVAZ:
-                return "Garsivaz";
-            case SHIR_GUYA:
-                return "Shir Guya";
-            case AMIR_ARSALAN:
-                return "Amir Arsalan";
-            case MALEK_SHAPOUR:
-                return "Malek Shapour";
-            case REIHANEH_JADOO:
-                return "Reihaneh Jadoo";
-        }
-        return "";
+        return PersonType.stringValue(type);
     }
 
     public abstract void getAction();
